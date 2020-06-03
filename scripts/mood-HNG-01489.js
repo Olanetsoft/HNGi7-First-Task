a63af100de834842831d5d768b76469387a13a2d
Ahmed Mansour <moodfactor@hotmail.com>
2020-06-03 19:07:19 +0200

    added my script

---
+++ scripts/mood-HNG-01489.js
@@ -0,0 +1,9 @@
+
+let name = "Ahmed Mansour";
+let hngID = "HNG-01489";
+let email = "moodfactor@gmail.com"
+let language = "Javascript";
+
+let result = `Hello World, this is ${name} with HNGi7 ID ${hngID} using ${language} email ${email} for stage2 task`;
+
+console.log(result);
