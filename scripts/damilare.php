d1cf1b384dcd467cd25b81bb51fdd37356460b2f
darehamed <42046888+darehamed@users.noreply.github.com>
2020-06-03 10:39:14 +0100

    Add files via upload

---
+++ scripts/damilare.php
@@ -0,0 +1,8 @@
+<?php
+	$name = "Damilare Hamed";
+	$nternship_id = "HNG-00251";
+	$language = "php";
+
+	echo "Hello World, this is " . $name . " with HNGi7 ID " . $nternship_id . " using " . $language . " for stage 2 task";
+
+?>
