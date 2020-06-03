a07aae2faebb42b6f63ee46231793c1a402af299
Ahmed Mansour <moodfactor@hotmail.com>
2020-06-03 18:47:16 +0200

    no conflicts

--- scripts/mood-HNG-01489.js
+++ scripts/mood-HNG-01489.js
@@ -4,6 +4,6 @@ let hngID = "HNG-01489";
 let email = "moodfactor@gmail.com"
 let language = "Javascript";
 
-let result = `Hello World, this is ${name} with HNGi7 ID ${hngID} using ${language} email ${email} for stage 2 task`;
+let result = `Hello World, this is ${name} with HNGi7 ID ${hngID} using ${language} email ${email} for stage2 task`;
 
 console.log(result);
