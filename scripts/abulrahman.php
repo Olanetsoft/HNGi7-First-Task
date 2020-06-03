c3f11e2389a6771a9c2ae33c95a2158b3fe144b3
Yusuf Abdulrahman <48928718+devYusuf@users.noreply.github.com>
2020-06-03 10:15:01 +0000

    Update and rename abulrahman.js to abulrahman.php

---
+++ scripts/abulrahman.php
@@ -0,0 +1,7 @@
+$name = "Yusuf Abdulrahman";
+$hngID = "HNG-06182";
+$language = "Javascript";
+
+$result = "Hello World, this is $name with HNGi7 ID $hngID using $language for stage 2 task";
+
+echo($result);
