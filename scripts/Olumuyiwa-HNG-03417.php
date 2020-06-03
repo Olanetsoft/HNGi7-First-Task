3fc9ab9e49cb00ed7ef407312d28da5b52760d25
wsticks <akannimuyiwa@gmail.com>
2020-06-03 18:35:10 +0100

    olumuyiwa's commit

---
+++ scripts/Olumuyiwa-HNG-03417.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$full_name = "Olumuyiwa Akanni";
+$ID = "HNG-03417";
+$language = "PHP";
+$Email = "akannimuyiwa@gmail.com";
+echo json_encode( "Hello World, this is " . $full_name. " with HNGi7 ID " . $ID .  " using " . $language . " for stage 2 task and " . $Email);
+//echo json_encode($print);
+	
+?>
+
+</body>
+</html>
