7969e76090eefc4c4a32b6bb7e971c4ed1df7ade
Ayodeji Oladimeji <alayosingers@gmail.com>
2020-06-03 11:16:22 +0100

    my script

---
+++ scripts/oladimeji_script.js
@@ -0,0 +1,8 @@
+
+let data = {
+    "data": "Hello World, this is AYODEJI OLADIMEJI with HNGi7 ID HNG-01047 and email alayosingers@gmail.com using JAVASCRIPT for stage 2 task"
+}
+
+let myJSON = JSON.stringify(data);
+
+console.log(myJSON);
