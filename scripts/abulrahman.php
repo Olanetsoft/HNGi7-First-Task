2d0cff61bd6e8bebbfa5b68bc4156fe7270af2a9
Yusuf Abdulrahman <48928718+devYusuf@users.noreply.github.com>
2020-06-03 10:19:14 +0000

    Update abulrahman.php

--- scripts/abulrahman.php
+++ scripts/abulrahman.php
@@ -1,7 +1,10 @@
+
+<?php
 $name = "Yusuf Abdulrahman";
 $hngID = "HNG-06182";
-$language = "Javascript";
+$language = "PHP";
 
 $result = "Hello World, this is $name with HNGi7 ID $hngID using $language for stage 2 task";
 
 echo($result);
+?>
