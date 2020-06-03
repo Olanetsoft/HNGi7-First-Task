98d9f73815244502b8a1f257e0995befb4ede80b
Yahaya Kehinde <mac@Yahaya-Kehinde-macbook.local>
2020-06-03 14:11:27 +0100

    i added my email

--- scripts/yhkehinde.js
+++ scripts/yhkehinde.js
@@ -1,14 +1,15 @@
 let person = {
+	email: "y.hkehinde@yahoo.com",
     fullName :"Yahaya Kehinde",
     hngID : "HNG-01939",
     progLanguage : "javascript",
-    email: "y.hkehinde@yahoo.com"
+   
 };
 let cl = console.log;
 
 
 const showText = () => {
-    cl(`Hello World, this is ${person.fullName} with HNGi7 ID ${person.hngID} using ${person.progLanguage} for stage 2 task`);
+    cl(`Hello World, this is ${person.fullName} with HNGi7 ID ${person.hngID} using ${person.progLanguage} for stage 2 task and ${person.email}`);
 };
 
 showText();
