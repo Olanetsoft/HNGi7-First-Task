ca7c5a5ce5ac22e1ee282546beebc27715eae235
Okekunle Oluwatobiloba <okekunletobi@gmail.com>
2020-06-03 17:13:27 +0100

    added my script andit works fine locally also

--- scripts/oluwatobiloba.js
+++ scripts/oluwatobiloba.js
@@ -1,9 +1,7 @@
-let content = "Hello World, this is Oluwatobiloba Okekunle with HNGi7 ID HNG-01385 using javascript for stage 2 task"
-let email = "okekunletobi@gmail.com";
+let content = "Hello World, this is Oluwatobiloba Okekunle with HNGi7 ID HNG-01385 using javascript for stage 2 task and okekunletobi@gmail.com"
 
 const oluwatobiloba_script = () => {
     console.log(content);
-    console.log(email);
 }
 
 oluwatobiloba_script();
