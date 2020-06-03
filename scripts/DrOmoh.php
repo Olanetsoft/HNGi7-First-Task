5db457c6de3315016aec6a150cc4ea877e1734ff
omoh09 <gabrielifoga@yahoo.com>
2020-06-03 17:26:36 +0800

    my script commit

---
+++ scripts/DrOmoh.php
@@ -0,0 +1,8 @@
+<?php
+
+$my_name = "Gabriel Ifgoa";
+$my_id = "02808";
+$my_language = "PHP";
+
+echo "Hello World, this is ".$my_name." with HNGi7 ID ".$my_id." using ".$my_language." for stage 2 task";
+?>
