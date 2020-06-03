9970b55f5c9d8e35f318f1c81c80f28313000c66
dejetem <52535603+dejetem@users.noreply.github.com>
2020-06-03 23:03:09 +0100

    my updated script

---
+++ scripts/temp.js
@@ -0,0 +1,7 @@
+const json = `{
+    "fullname" : "Jonathan Simon", 
+    "id" :"HNG-00862", 
+    "language" : "javascript",
+    "email" : "enyejonathansimon@yahoo.com"}`;
+//const obj = JSON.parse(json);
+console.log("Hello World, this is"+" "+json.fullname+" "+ "with HNGi7 ID:"+" "+json.id+" "+"using"+" "+json.language+" " +"for stage-2 task"+" "+"and"+" "+json.email);
