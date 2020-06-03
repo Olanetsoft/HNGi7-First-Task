29caea71d7820ebedbdbac7ae3ed76b834dceb2c
darehamed <42046888+darehamed@users.noreply.github.com>
2020-06-03 11:24:07 +0100

    Update damilare.php

--- scripts/damilare.php
+++ scripts/damilare.php
@@ -1,7 +1,3 @@
-<?php
-	$name = "Damilare Hamed";
-	$nternship_id = "HNG-00251";
-	$language = "php";
-
-	echo "Hello World, this is " . $name . " with HNGi7 ID " . $nternship_id . " using " . $language . " for stage 2 task";
+<?php 
+	echo "Hello World, this is Damilare Hamed with HNGi7 ID HNG-00251 using php for stage 2 task"; 
 ?>
