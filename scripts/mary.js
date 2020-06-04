88babcd9d98b28e7a20fa9647322fde8cf8220eb
Orevvy <morevaoghene@gmail.com>
2020-06-04 07:18:45 +0100

    adjust mary file

--- scripts/mary.js
+++ scripts/mary.js
@@ -1,4 +1,4 @@
-function mary1 () {
+function intern_info(){
     return "Hello World, this is Enoch Mary Orevaoghene with HNGi7 ID HNG-00484 using javascript for stage 2 task and morevaoghene@gmail.com";
 }
-console.log(mary1())
+console.log(intern_info())
