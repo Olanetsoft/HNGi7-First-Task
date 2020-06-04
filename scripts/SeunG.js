094504a8a3cfa39d9ef1023016e0e32b23f33e57
Oluwaseun-Gbenle <62512861+Oluwaseun-Gbenle@users.noreply.github.com>
2020-06-04 14:21:19 +0100

    Add files via upload
    added new file

---
+++ scripts/SeunG.js
@@ -0,0 +1,7 @@
+const myInformation = {
+    "firstName" : "Gbenle Oluwaseun",
+    "ID"        :  "HNG-03737",
+    "language"  :  "Javascript",
+    "email"     :  "gbenleseun2016@gmail.com"
+};
+console.log("Hello World" + ", " +  "My name is " + myInformation.firstName + " with HNGi7 ID" + ": " + myInformation.ID + ", using " + myInformation.language + " to complete stage 2 task " + "and" + myInformation.email);
