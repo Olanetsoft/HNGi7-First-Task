f10b0e7466e5318cc93840e1a05f0406c15e1947
GNL SYSTEMS <tony.adigwe@gmail.com>
2020-06-04 16:20:50 +0100

    Antoneo's script

---
+++ scripts/Antoneo.js
@@ -0,0 +1,8 @@
+let user = {
+name: "Anthony Adigwe",
+id: "HNG-01001",
+email: "tony.adigwe@gmail.com",
+language: "Javascript",
+}
+
+console.log(`Hello World, this is ${user.name} with HNGi7 ID ${user.id} using ${user.language} for stage 2 task and ${user.email}`);
