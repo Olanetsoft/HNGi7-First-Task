5e12e412d408979cf6a1bb033d94e5f4509c6918
BusariSherif <66230294+BusariSherif@users.noreply.github.com>
2020-06-04 18:44:12 +0100

    Update and rename OmniMontana_HNG-03098.js to OmniMontana.js

---
+++ scripts/OmniMontana.js
@@ -0,0 +1,7 @@
+// A javascript that program will print my detail for HNG task 2 team granite
+
+//storing result into a variable
+
+let output = 'Hello World, this is Busari Sherif with HNGi7 ID HNG-03098 using JavaScript for stage 2 task email busari.sherif.o@gmail.com';
+
+console.log(output) //printing result
