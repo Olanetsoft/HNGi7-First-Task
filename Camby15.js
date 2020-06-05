850eaabe1c70ba4ad34bf4a56f81effabe2f9f6e
camby15 <omorimbaram@gmail.com>
2020-06-05 19:27:13 +0100

    camby15

---
+++ Camby15.js
@@ -0,0 +1,6 @@
+var email = "omorimbaram@gmail.com";
+var firstName = "Mbaram";
+var lastName = "Omori";
+var ID = "HNG-06331";
+var resultString = `Hello World, this is ${firstName} ${lastName} with HNGi7 ID ${ID} using Javascript for stage 2 task and ${email}`
+console.log(resultString);
