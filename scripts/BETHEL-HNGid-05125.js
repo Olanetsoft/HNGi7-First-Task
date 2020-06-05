9d2a93adcad796b3e88da72da90c189c31539197
Osahon Bethel <bethelosahon@gmail.com>
2020-06-05 14:38:36 +0100

    Update BETHEL-HNGid-05125.js

--- scripts/BETHEL-HNGid-05125.js
+++ scripts/BETHEL-HNGid-05125.js
@@ -5,5 +5,5 @@
     language = "Javascript",
     email = "bethelosahon@gmail.com"
  
-let myData =  `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email}`;
+let myData =  `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task and ${email}`;
 console.log(myData);
