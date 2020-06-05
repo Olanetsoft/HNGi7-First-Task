5aee6c837d13c8d8da598afb6d4ad02131858e95
Emmanuel Doji <dojiemma@gmail.com>
2020-06-05 09:38:33 +0100

    doji js

---
+++ doji js.js
@@ -0,0 +1,9 @@
+var info = "Hello World, this is Emmanuel Doji with HNGi7 ID HNG-02005 using Javascript for stage 2 task and ";
+var email = "emmadoji@gmail.com";
+var hng_id = "HNG-02005";
+var language = "JavaScript";
+
+var mystr = `${info} ${email} ${hng_id} ${language}`
+
+
+console.log(mystr);
