8029d3353672ae2ebc56f8a6415a6799731c6c25
Davidson <Davidbrain162@gmail.com>
2020-06-05 18:15:24 +0100

    feat :added Davida.js

---
+++ scripts/davida.js
@@ -0,0 +1,11 @@
+// Hello World, this is fullname with HNGi7 ID [ID]
+const fullName = "Davidson Ayo";
+const hngi7ID = "HNG-06359";
+const email = "Davidbrain162@gmail.com";
+
+introFunction = () => {
+  const intro = `Hello World, this is ${fullName} with HNGi7 ID ${hngi7ID} using Javascript for stage 2 task email ${email}`;
+  return console.log(intro);
+};
+
+introFunction();
