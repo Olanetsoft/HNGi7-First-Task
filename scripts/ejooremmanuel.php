d599f01506f92d5e2893370b901f477a743ff8a3
ejooremmanuel <55317332+ejooremmanuel@users.noreply.github.com>
2020-06-05 12:32:16 +0100

    script for ejooremmanuel

---
+++ scripts/ejooremmanuel.php
@@ -0,0 +1,14 @@
+<?php
+
+	$firstname = "Emmanuel";
+	$lastname = "Ejoor";
+	$language = "php";
+	$id = "HNG-04434";
+	$email = "ejooremmanuel@gmail.com";
+
+	//$data = array('Emmanuel','Ejoor','HNG-04434','php','ejooremmanuel@gmail.com' );
+
+
+	echo "Hello World, this is $lastname $firstname with HNGi7 ID $id using $language for stage 2 task and $email";
+
+?>
---
+++ scripts/ejooremmanuel.py
@@ -0,0 +1,12 @@
+
+firstname= "Emmanuel"
+
+Lastname = "Ejoor"
+
+internid = "HNG-04434"
+
+language = "PHP"
+
+email = "ejooremmanuel@gmail.com"
+
+print('Hello World, this is Ejoor Emmanuel with HNGi7 ID HNG-04434 using python for stage 2 task and ejooremmanuel@gmail.com')
