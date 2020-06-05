725ccb0d40b531690054a22e057ee4606edb0292
kennyendowed <kenneyg50@gmail.com>
2020-06-05 20:27:13 +0100

    up2

--- scripts/kennyendowed-05471.php
+++ scripts/kennyendowed-05471.php
@@ -1,13 +1,17 @@
 <?php
-$name = "kenneth Akpan";
-$hng_id = "HNG-05471";
-$language = "PHP";
-$email = "kenneyg50@gmail.com";
+
 
 function show_info(){
-    global  $hng_id, $language, $email;
+  $name = "kenneth Akpan";
+  $hng_id = "HNG-05471";
+  $language = "PHP";
+  $email = "kenneyg50@gmail.com";
     return "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
 }
 
-echo show_info();
+//print "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task and $email";
+
+
+
+ echo show_info();
 ?>
