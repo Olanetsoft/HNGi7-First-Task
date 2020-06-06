34e1a8e4ccf49326c453ec598a1a516d24094cf0
Dani-code-opsC <57980851+Dani-code-opsC@users.noreply.github.com>
2020-06-06 15:32:00 +0100

    updated version

--- scripts/amoodaniel_.py
+++ scripts/amoodaniel_.py
@@ -3,5 +3,5 @@ hng_id = 'HNG-02816'
 name = 'Amoo Daniel'
 lang= 'Python'
 mail = 'danielamoo35@gmail.com'
-print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task and {3}". format(name, id, lang, mail))
+print("Hello World, this is {0} with HNGi7 ID {1} using {2} for stage 2 task and {3}". format(name, hng_id, lang, mail))
 
