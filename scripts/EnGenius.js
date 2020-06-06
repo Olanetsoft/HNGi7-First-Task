a7eee7bb298875007354b9a8f0b6bad2754ca28c
EnGeniuss <64276263+EnGeniuss@users.noreply.github.com>
2020-06-06 22:28:55 +0100

    Create EnGenius.js

---
+++ scripts/EnGenius.js
@@ -0,0 +1,4 @@
+var language='Javascript';
+var Name='Muhammed Yakub';
+var ID='HNG06594';
+console.log('Hello World, this is'+Name+'with HNGI7 ID'+ID+'using'+language+'for stage 2 task and engeniusss@gmail.com');
