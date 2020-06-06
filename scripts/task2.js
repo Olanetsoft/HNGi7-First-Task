6e2941f66e6a402d401905a19eadb3383ef963ab
EnGeniuss <64276263+EnGeniuss@users.noreply.github.com>
2020-06-06 22:24:23 +0100

    Update task2.js

--- scripts/task2.js
+++ scripts/task2.js
@@ -1,5 +1,4 @@
 var language='Javascript';
 var Name='Muhammed Yakub';
-var ID='HNG06594';
-var email='engeniusss@gmail.com';
-console.log('Hello World, This is'+Name+'with HNGI7 ID'+ID+'using'+language+'for stage 2 task and email address'+email);
+var ID='HNG06594'; 
+console.log('Hello World, this is'+Name+'with HNGI7 ID'+ID+'using'+language+'for stage 2 task and engeniusss@gmail.com';
